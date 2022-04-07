dba9b677d639576f4837db824b78b0fcfdffe569
SatpreetMakhija <sm.satpreet@gmail.com>
2022-04-07 15:01:59 +0530

    added Home page

--- src/App.js
+++ src/App.js
@@ -1,55 +1,63 @@
-import { useState } from 'react';
-import { ethers } from 'ethers';
-import './App.css';
-import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+import { useState } from "react";
+import { ethers } from "ethers";
+import "./App.css";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+
+//import React components
+import Home from "./Components/Pages/Home";
 
 const greeterAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 
 function App() {
-
-  const [greeting, setGreetingValue] = useState('')
+  const [greeting, setGreetingValue] = useState("");
 
   async function requestAccount() {
-    await window.ethereum.request({method: 'eth_requestAccounts'});
+    await window.ethereum.request({ method: "eth_requestAccounts" });
   }
 
   async function fetchGreeting() {
-    //checks whether Metamask is installed on the browser or not. 
-    if (typeof window.ethereum !== 'undefined'){
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+    //checks whether Metamask is installed on the browser or not.
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
       try {
-        const data = await contract.greet()
-        console.log('data: ', data)
+        const data = await contract.greet();
+        console.log("data: ", data);
       } catch (err) {
-        console.log("Error: ", err)
+        console.log("Error: ", err);
       }
     }
   }
 
   async function setGreeting() {
-    if (!greeting) return 
-    if (typeof window.ethereum !== 'undefined'){
-      await requestAccount()
+    if (!greeting) return;
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
-      const transaction = await contract.setGreeting(greeting)
-      setGreetingValue('')
-      await transaction.wait()
-      fetchGreeting()
+      const transaction = await contract.setGreeting(greeting);
+      setGreetingValue("");
+      await transaction.wait();
+      fetchGreeting();
     }
   }
 
-
   return (
     <div className="App">
-      <header className='App-header'>
+      <Home />
+      <header className="App-header">
         <button onClick={fetchGreeting}>Fetch Greeting</button>
         <button onClick={setGreeting}>Set Greeting</button>
-        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" value={greeting}/>
-
-
+        <input
+          onChange={(e) => setGreetingValue(e.target.value)}
+          placeholder="Set greeting"
+          value={greeting}
+        />
       </header>
     </div>
   );
