0fced04a7f6f75a9b2e289f10e3c6641c1474fd2
SatpreetMakhija <sm.satpreet@gmail.com>
2022-04-08 15:47:47 +0530

    started working on adding global Store for data state management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ethers } from "ethers";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,59 +7,66 @@ import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
 //import React components
 import Home from "./Components/Pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UploadDoc from './Components/Pages/UploadDoc'
+import UploadDoc from "./Components/Pages/UploadDoc";
 //import React-bootstrap elements
 import { Button } from "react-bootstrap";
 import NavigationBar from "./Components/Shared Elements/Navbar";
 import ValidateDoc from "./Components/Pages/ValidateDoc";
 import Login from "./Components/Pages/Login";
 
+//global state management using Redux-toolkit
+import { useSelector, useDispatch } from "react-redux";
+import { loginUser, logoutUser } from "./features/login/loginSlice";
+
 const greeterAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 
 function App() {
-  const [greeting, setGreetingValue] = useState("");
+  
+  const isUserLoggedIn = useSelector((state) => state.login.isUserLoggedIn);
+  
+  // const [greeting, setGreetingValue] = useState("");
 
-  async function requestAccount() {
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-  }
+  // async function requestAccount() {
+  //   await window.ethereum.request({ method: "eth_requestAccounts" });
+  // }
 
-  async function fetchGreeting() {
-    //checks whether Metamask is installed on the browser or not.
-    if (typeof window.ethereum !== "undefined") {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const contract = new ethers.Contract(
-        greeterAddress,
-        Greeter.abi,
-        provider
-      );
-      try {
-        const data = await contract.greet();
-        console.log("data: ", data);
-      } catch (err) {
-        console.log("Error: ", err);
-      }
-    }
-  }
+  // async function fetchGreeting() {
+  //   //checks whether Metamask is installed on the browser or not.
+  //   if (typeof window.ethereum !== "undefined") {
+  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //     const contract = new ethers.Contract(
+  //       greeterAddress,
+  //       Greeter.abi,
+  //       provider
+  //     );
+  //     try {
+  //       const data = await contract.greet();
+  //       console.log("data: ", data);
+  //     } catch (err) {
+  //       console.log("Error: ", err);
+  //     }
+  //   }
+  // }
 
-  async function setGreeting() {
-    if (!greeting) return;
-    if (typeof window.ethereum !== "undefined") {
-      await requestAccount();
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
-      const transaction = await contract.setGreeting(greeting);
-      setGreetingValue("");
-      await transaction.wait();
-      fetchGreeting();
-    }
-  }
+  // async function setGreeting() {
+  //   if (!greeting) return;
+  //   if (typeof window.ethereum !== "undefined") {
+  //     await requestAccount();
+  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //     const signer = provider.getSigner();
+  //     const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+  //     const transaction = await contract.setGreeting(greeting);
+  //     setGreetingValue("");
+  //     await transaction.wait();
+  //     fetchGreeting();
+  //   }
+  // }
 
   return (
     <React.Fragment>
-      <div className="App">
-        
-        {/* <header className="App-header">
+      
+        <div className="App">
+          {/* <header className="App-header">
           <button onClick={fetchGreeting}>Fetch Greeting</button>
           <button onClick={setGreeting}>Set Greeting</button>
           <input
@@ -69,18 +76,17 @@ function App() {
           />
         
         </header> */}
-        <Router>
-        <NavigationBar/>
-          <Routes>
-            <Route  path="/" element={<Home/>}/>
-            <Route path="/upload-doc" element={<UploadDoc/>}/>
-            <Route path="/validate-doc" element={<ValidateDoc/>}/>
-            <Route path="/login" element={<Login/>}/>
-          </Routes>
-        </Router>
-
-
-      </div>
+          <Router>
+            <NavigationBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/upload-doc" element={<UploadDoc />} />
+              <Route path="/validate-doc" element={<ValidateDoc />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </Router>
+        </div>
+      
     </React.Fragment>
   );
 }
