60a010bdc031e4ab560f925dbd13e93e417876e0
SatpreetMakhija <sm.satpreet@gmail.com>
2022-04-21 21:14:37 +0530

    created FileUpload.sol & changed deploy.js

---
+++ contracts/FileUpload.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract FileUpload {
+    string private docHash;
+
+    // constructor(string memory _docHash) {
+    //     console.log("Deploying a Greeter with greeting:", _docHash);
+    //     docHash = _docHash;
+    // }
+
+    function getDocHash() public view returns (string memory) {
+        return docHash;
+    }
+
+    function setDocHash(string memory _docHash) public {
+        console.log("Changing greeting from '%s' to '%s'", docHash, _docHash);
+        docHash = _docHash;
+    }
+}
+
